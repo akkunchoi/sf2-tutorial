6f9aa7a73001523f1724806caaf5981a6ebede15
Akiyoshi Tanaka <akkunchoi@gmail.com>
2012-04-06 20:52:00 +0900

    tutorial finish!

---
+++ Symfony/src/My/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace My\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use My\BlogBundle\Entity\Post;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $posts = $em->getRepository('MyBlogBundle:Post')->findAll();
+        return $this->render('MyBlogBundle:Default:index.html.twig', array('posts' => $posts));
+    }
+
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        return $this->render('MyBlogBundle:Default:show.html.twig', array('post' => $post));
+    }
+    public function newAction()
+    {
+        // フォームのビルド
+        $form = $this->createFormBuilder(new Post())  // ここでPostクラスを使うため、ファイルの先頭あたりにuseを追加していることに注意
+            ->add('title')
+            ->add('body')
+            ->getForm();
+
+        // バリデーション
+        $request = $this->getRequest();
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                // エンティティを永続化
+                $post = $form->getData();
+                $post->setCreatedAt(new \DateTime());
+                $post->setUpdatedAt(new \DateTime());
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($post);
+                $em->flush();
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        // 描画
+        return $this->render('MyBlogBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        if (!$post) {
+            throw new NotFoundHttpException('The post does not exist.');
+        }
+        $em->remove($post);
+        $em->flush();
+        return $this->redirect($this->generateUrl('blog_index'));
+    }
+    public function editAction($id)
+    {
+        // DBから取得
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('MyBlogBundle:Post', $id);
+        if (!$post) {
+            throw new NotFoundHttpException('The post does not exist.');
+        }
+
+        // フォームのビルド
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('body')
+            ->getForm();
+
+        // バリデーション
+        $request = $this->getRequest();
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+                // 更新されたエンティティをデータベースに保存
+                $post = $form->getData();
+                $post->setUpdatedAt(new \DateTime());
+                $em->flush();
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        // 描画
+        return $this->render('MyBlogBundle:Default:edit.html.twig', array(
+            'post' => $post,
+            'form' => $form->createView(),
+        ));
+    }    
+}
